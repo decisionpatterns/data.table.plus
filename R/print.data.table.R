5d1e153115d49c6b1a9eb5a45a0c1d52b5a4912a
Christopher Brown <cbrown@decisionpatterns.com>
2015-01-31 07:00:38 -0800

    Version 0.1.1
    - Fix print.data.table to collapse keys

--- R/print.data.table.R
+++ R/print.data.table.R
@@ -1,30 +1,30 @@
 #' print.data.table
-#' 
-#' Extends \code{\link[data.table]{print}} by printing x followed by it's key
-#' 
+#'
+#' Extends \code{\link[data.table]{print}} by printing x followed by its key
+#'
 #' @param x data.table
-#' @param ... arguments passed to 
-#' 
+#' @param ... arguments passed to
+#'
+#'
 #' @return NONE, used for side effects only
-#' 
-#' @examples 
+#'
+#' @examples
 #'   data(iris)
 #'   setDT(iris)
-#'   
+#'
 #'   print(iris)
-#'   
+#'
 #'   setkey(iris, Species )
 #'   print(iris)
-#'   
+#'
 #' @import data.table
 #' @export
 
-print.data.table <- function( x, ... ) { 
-   
+print.data.table <- function( x, ... ) {
+
   data.table:::print.data.table( x=x,  ... )
-  message( paste( "key :", data.table::key(x) ) ) 
-  
+  message( paste( "key :", paste0( data.table::key(x), collapse = ", ")) )
+
   invisible()
-  
+
 }
-   
