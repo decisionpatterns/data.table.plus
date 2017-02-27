6f6934119ac633049ade65fd2e3c192d74946d15
Christopher Brown <cbrown@decisionpatterns.com>
2017-02-27 06:50:57 -0800

    Add by_list.R

---
+++ R/by_list.R
@@ -0,0 +1,17 @@
+#' Turn a list of variables into a by list
+#'
+#' @return 
+#'  
+#' names list; values are the names 
+#'  
+#' @examples 
+#' 
+#' mpg %>% by_list( c("year","trans" ) )
+#' 
+#' @note belongs in data.table.plust
+#' @export 
+
+by_list <- function(.data, x) { 
+  .data[ , x, with=FALSE  ] %>% as.list
+}
+
