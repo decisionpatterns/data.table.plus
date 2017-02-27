e9dc7600f77aeea5686e56529675f595905c9758
Christopher Brown <cbrown@decisionpatterns.com>
2017-02-27 06:50:12 -0800

    Add i_table

---
+++ R/i_table.R
@@ -0,0 +1,10 @@
+#' Create a table for use in data.tables i slot
+#' 
+#' @param .data data.table 
+#' @param x character; name of character columns
+
+i_table <- function(.data, x) { 
+  .data[, x, with=FALSE ]  
+}
+
+
