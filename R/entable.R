c09bf9d75e02f2a39f841dddd39af8eb56c5bbf6
Christopher Brown <cbrown@decisionpatterns.com>
2018-12-14 08:58:12 -0800

    Update entable()

--- R/entable.R
+++ R/entable.R
@@ -1,13 +1,24 @@
-#' Convert vectors to data frames and vice versa
+#' Convert vectors to data tables and vice versa
 #'
-#' @param x atomic vector (named)
-#' @param name,value Names of the columns that store the names and values.
-#'   If `name` is `NULL`, a one-column tibble is returned; `value` cannot be `NULL`.
+#' @param x atomic vector
+#' @param key,value Names of the columns that store the keys and values.
 #'
-#' @return A [data.table] with two columns (if `name` is not `NULL`, the default)
-#'   or one column (otherwise).#'
+#' @details
+#'
+#' `entable` converts a data.table from an atomic vector. The vector becomes a
+#' table column named `value` which, by default, is named after `x`.
+#'
+#'  If the vector is named, a two-column data table is produced. The first
+#'  column is the names of x and is named by the `key` argument. The key
+#'  defaults to `.key`.
+#'
+#'  A key is created for the first column.
+#'
+#' @return A one or two column [data.table] with a key on the first column.
+#'
 #' @seealso
-#'   `tibble::enframe()`
+#'   - [data.table::setkey()]
+#'   - `tibble::enframe()` - from which this function is loosely inspired.
 #'
 #' @examples
 #'   x = 1:3
@@ -20,7 +31,7 @@
 #'
 #' @export
 
-entable <- function(x, name = ".key", value=as.character(substitute(x)) ) {
+entable <- function(x, key = ".key", value=as.character(substitute(x)) ) {
 
   if( length(value) > 1 )
     value <- paste0(value, collapse="")
@@ -28,10 +39,11 @@ entable <- function(x, name = ".key", value=as.character(substitute(x)) ) {
   value <- make.names(value)
 
   dt <- as.data.table(x, keep.rownames = TRUE)
+
   if( ncol(dt) == 1 )
       setnames(dt, names(dt), value ) else
   if( ncol(dt) == 2 )
-      setnames(dt, names(dt), c(name,value) ) else
+      setnames(dt, names(dt), c(key,value) ) else
   stop("entable does not support multi-dimensional arrays")
 
   setkeyv(dt, names(dt)[[1]] )
